8e96672537938ce51ea6cc69d47563186dd0bf6f
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-24 16:22:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,14 @@ def summon_captain_planet(planeteer)# code an argument here
   new_planeteer
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  i = 0
+  if calls.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+    i = i +1  
+  end
 end
 
 def find_the_cheese# code an argument here
