031dcc9a1c6dd7edb2d94a3f7d03a54f10f0f6df
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-24 16:10:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
-def roll_call_dwarves(dwarves)# code an argument here
-  roll_call_dwarves = []
-  dwarves.each_with_index do |dwarf|
-    roll_call_dwarves << dwarf
+def roll_call_dwarves(array)# code an argument here
+  i = 0
+  while i < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
   end
 end
 
