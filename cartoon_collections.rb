375efbc0ac9187b2611df2afe044a5605c295aba
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-24 16:04:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves9(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf|
+    dwarf
+  end  
 end
 
 def summon_captain_planet# code an argument here
